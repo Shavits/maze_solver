6a50713372968c7c026a87815468eb5245d13313
shavits <s.shavit4@gmail.com>
2025-06-01 11:12:59 +0300

    added draw_move

--- cell.py
+++ cell.py
@@ -36,3 +36,18 @@ class Cell():
                 Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)), "black"
             )
 
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo:
+            color = "grey"
+        self_center = Point(
+            (self.__x1 + self.__x2) / 2, (self.__y1 + self.__y2) / 2
+        )
+        to_center = Point(
+            (to_cell.__x1 + to_cell.__x2) / 2, (to_cell.__y1 + to_cell.__y2) / 2
+        )
+        self.__win.draw_line(
+            Line(self_center, to_center), color
+        )
+        
+
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     cell2 = Cell(win)
     cell1.draw(Point(100, 100), Point(200, 200))
     cell2.draw(Point(200, 100), Point(300, 200))
+    cell1.draw_move(cell2, True)
+
     win.wait_for_close()
 
 if __name__ == "__main__":
